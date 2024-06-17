520d7e72e24377929b8209fe154dad3fd8cd2673
WhyFenceCode <120431739+WhyFenceCode@users.noreply.github.com>
2024-06-17 09:48:54 -0700

    Update questions.js

--- questions.js
+++ questions.js
@@ -75,41 +75,21 @@ let a200 = [
 ]
 
 let q300 = [
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
+  "During what procedure is the MLV moving at it’s slowest?",
+  "What is the procedure called when the Skycrane drops free of the MLV?",
+  "When does the Skycrane maneuver begin?",
+  "Before the Skycrane maneuver begins what speed does the MLV decelerate to?",
+  "Is it environmentally friendly to send MLV’s to mars?",
+  "What will the engine do if it goes bellow 1% throttle?"
 ]
 
 let a300 = [
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
+  "Skycrane Maneuver",
+  "Back shell separation",
+  "The Skycrane maneuver begins when the MLV is around 20 meters from the ground",
+  "The MLV decelerates itself to a speed of 2km/hour",
+  "We feel that it is not, but neither are any other solutions humanity has for sending rovers to mars",
+  "It will shut down"
 ]
 
 let q400 = [
